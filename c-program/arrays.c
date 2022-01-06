054e878c598d2ab7dcd497c3df126a2f6a8998d0
st9c <89186757+st9c@users.noreply.github.com>
2022-01-06 09:15:27 +0100

    Create arrays.c

---
+++ c-program/arrays.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+  string s = ["Person A", "Person B", "Person C etc..."];
+  string input = get_string("Type the Name of a Person:  ");
+  for(int i = 0; i < s; i++)
+  {
+    if(input == s[])
+    {
+      printf("Person is Found.);
+    }
+    else
+    {
+      printf("Person is not found.);
+  }
+}
